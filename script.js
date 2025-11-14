6a4722785c1bc1bdf1b21648584d4bbf2e002319
Sebastien Humeau <sebhumeau@free.fr>
2025-11-14 18:58:31 +0100

    Première version des fichiers

---
+++ script.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    let   $mainMenuItems = $("#main-menu ul").children("li"),
+            totalMainMenuItems = $mainMenuItems.length,
+            openedIndex = 2,
+            init = function () {
+                bindEvents();
+
+                if(validIndex(openedIndex)) {
+                    animateItem($mainMenuItems.eq(openedIndex),true,700);
+                }
+            },
+
+            bindEvents = function() {
+                $mainMenuItems.children(".images").click(function() {
+                    let newIndex = $(this).parent().index();
+                    checkAndAnimateItem(newIndex);
+                });
+
+                $(".button").hover(
+                function() {
+                    $(this).addClass("hovered");
+                },
+                function() {
+                    $(this).removeClass("hovered");
+                });
+
+                $(".button").click(function() {
+                    let newIndex = $(this).index();
+                    checkAndAnimateItem(newIndex);              
+                });
+            },
+
+            validIndex = function(indexToCheck) {
+                return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
+            },
+
+            animateItem = function($item, toOpen, speed){
+                let $colorImage = $item.find(".color"),
+                $description = $item.find(".description"),
+                itemParam = toOpen ? {width:"420px"} : {width:"140px"},
+                colorImageParam = toOpen ? {left: "0px"} : {left : "140px"};
+                descriptionParam = toOpen ? {left: "140px"} : {left : "420px"};
+
+                $colorImage.animate(colorImageParam,speed);
+                $item.animate(itemParam,speed);
+                $description.animate(descriptionParam, speed);
+            };
+
+            checkAndAnimateItem = function (indexToCheckAndAnimate) {           
+                    if(openedIndex === indexToCheckAndAnimate) {
+                        animateItem($mainMenuItems.eq(indexToCheckAndAnimate),false,250);
+                        openedIndex=-1;
+                    } else {
+                        if(validIndex(indexToCheckAndAnimate)) {
+                            animateItem($mainMenuItems.eq(openedIndex),false,250);
+                            openedIndex=indexToCheckAndAnimate;
+                            animateItem($mainMenuItems.eq(openedIndex),true,250);
+                        }
+                    }
+            };
+
+            init();
+});
